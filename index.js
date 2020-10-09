e11c4edb13b4c2721ac7f9b102e3e1a58eed3195
Jill Kay <jillannkay@gmail.com>
2020-10-09 15:15:54 +0200

    Remove User class to another file

--- index.js
+++ index.js
@@ -1,32 +1,3 @@
-class User {
-    constructor(name) {
-        this.name = name;
-        this.premium = false;
-        this.testedInterests = [];
-        this.starredInterests = [];
-    }
-
-    buyPremium() {
-        this.premium = true;
-    }
-
-    createInterest(name) {
-        return new Interest(name);
-    }
-
-    testInterest(interest) {
-        this.testedInterests.push(interest);
-        interest.tested = true;
-    }
-
-    starInterest(interest) {
-        this.starredInterests.push(interest);
-        interest.starred = true;
-    }
-
-}
-
-
 
 const jill = new User('Jill');
 const regina = new User('Regina');

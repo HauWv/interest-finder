9db2e067eebff9c7aaab93df6b86415a82991c31
Jill-Ann <jillannkay@gmail.com>
2020-10-16 17:30:35 +0200

    Add and star new interests

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ require('./project')
 
 const jill = new User('Jill')
 const regina = new User('Regina')
-const steve = new User('Steve')
+const steve = new User('Steve', 'stephen.a.ingram@gmail.com')
 
 // create interests
 const jillCoffee = jill.createInterest('coffee')
@@ -14,6 +14,7 @@ const steveHackers = steve.createInterest('hackers')
 const reginaVeganFood = regina.createInterest('vegan food')
 const reginaIceCream = regina.createInterest('ice cream')
 const reginaBananaBread = regina.createInterest('banana bread')
+const steveCoffee = steve.createInterest('coffee')
 
 // test interests
 jill.testInterest(jillCoffee)
@@ -24,6 +25,7 @@ steve.testInterest(steveHackers)
 regina.starInterest(reginaTea)
 jill.starInterest(jillCoffee)
 jill.starInterest(jillChocolate)
+steve.starInterest(steveCoffee)
 
 // create projects
 const jillProject = jill.createProject('jill project', 'new client', 'bicycles')

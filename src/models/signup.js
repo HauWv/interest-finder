9d90ebaa7e0c842367a526ac18c36bfb4e7a2e68
Jill-Ann <jillannkay@gmail.com>
2020-10-19 19:17:13 +0200

    Change signup class to include a signup method

--- src/models/signup.js
+++ src/models/signup.js
@@ -2,6 +2,12 @@ const User = require('./user')
 
 class SignUp {
   constructor(name, email, password) {
+    this.name = name
+    this.email = email
+    this.password = password
+  }
+
+  signUp(name, email, password) {
     return new User(name, email, password)
   }
 }

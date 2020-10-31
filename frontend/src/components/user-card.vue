c6a53be75e38ab49275a3194caf4f42b9371bafe
Jill-Ann <jillannkay@gmail.com>
2020-10-31 20:09:50 +0100

    Create a user-card component

---
+++ frontend/src/components/user-card.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  name: 'UserCard',
+  props: ['user']
+}
+</script>
+
+<template lang="pug">
+.box
+    h1 {{user.name}} 
+    h2 Projects ({{user.projects.length}})
+
+    .projects(v-for="project in user.projects")
+        h3 {{project.name}}
+    h2 Favourites:
+
+    .favourites(v-for="interest in user.starredInterests")
+        h3 {{interest.name}}
+</template>
+
+<style lang="scss" scoped>
+.box {
+  padding: 2rem;
+  border: 1px solid black;
+}
+</style>

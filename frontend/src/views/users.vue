368d59256e6b7852c4b2b88536c776ab3664786f
Jill-Ann <jillannkay@gmail.com>
2020-11-02 21:36:50 +0100

    Import mapActions to users view

--- frontend/src/views/users.vue
+++ frontend/src/views/users.vue
@@ -2,6 +2,7 @@
 // @ is an alias to /src
 import axios from 'axios'
 import UserCard from '@/components/user-card.vue' // @ means we are referencing the source directory.
+import { mapActions } from 'vuex'
 
 export default {
   name: 'Users',

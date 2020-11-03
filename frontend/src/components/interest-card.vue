aad777a161055b0b4367a5d7c028a7adb951a612
Jill-Ann <jillannkay@gmail.com>
2020-11-03 22:43:38 +0100

    Create interest-card component

---
+++ frontend/src/components/interest-card.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: 'InterestCard',
+  props: ['interest']
+}
+</script>
+
+<template lang="pug">
+.box
+    h1 {{interest.name}} 
+</template>
+
+<style lang="scss" scoped>
+.box {
+  padding: 2rem;
+  border: 1px solid black;
+}
+</style>

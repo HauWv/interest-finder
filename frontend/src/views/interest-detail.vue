e7d2d4a4a7b17973937c28492c19c4fc6f4dbd83
Jill-Ann <jillannkay@gmail.com>
2020-11-03 22:43:12 +0100

    Create interest detail component

---
+++ frontend/src/views/interest-detail.vue
@@ -0,0 +1,26 @@
+<script>
+import InterestCard from '@/components/interest-card.vue'
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'InterestDetail',
+  components: { InterestCard },
+  data() {
+    return {
+      interest: null // why do we need this as null?
+    }
+  },
+  async created() {
+    this.interest = await this.fetchStarredInterest(this.$route.params.id)
+  },
+  methods: {
+    ...mapActions(['fetchStarredInterest'])
+  }
+}
+</script>
+
+<template lang="pug">
+  div
+    h1 This is an interest detail
+    InterestCard(:interest="interest" v-if="interest")
+</template>

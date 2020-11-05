6fc16ae8cc6e8432c1db88692f5a4cf7d9f740b0
Jill-Ann <jillannkay@gmail.com>
2020-11-05 11:34:22 +0100

    Change starred view to interest-list

---
+++ frontend/src/views/interest-list.vue
@@ -0,0 +1,35 @@
+<script>
+import axios from 'axios'
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'Interests',
+  components: {},
+  data() {
+    return {
+      interests: []
+    }
+  },
+
+  async created() {
+    console.log('created')
+    this.interests = await this.fetchStarredInterests()
+  },
+  async beforeCreate() {
+    console.log('before create')
+  },
+
+  methods: {
+    ...mapActions(['fetchStarredInterests', 'toggleStarred'])
+  }
+}
+</script>
+
+<template lang="pug">
+  .box
+    h1 Starred Interests
+    div(v-for="interest in interests")
+      router-link(:to="`/interest-list/${interest._id}`") {{ interest.name }}
+      button(@click="toggleStarred(interest)") star / unstar
+      span {{ interest.starred }}
+</template>

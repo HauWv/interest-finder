b522392f0e4c9669ce5e92c948611fe2286b768b
Jill-Ann <jillannkay@gmail.com>
2020-11-05 11:38:39 +0100

    Add owner field to interest schema

--- backend/src/models/interest.js
+++ backend/src/models/interest.js
@@ -4,6 +4,9 @@ const interestSchema = new mongoose.Schema({
   name: {
     type: String,
     unique: true,
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
     required: true,
   },
   tested: {

457515e48cb2093515417617c44a28538b1c1a06
Jill-Ann <jillannkay@gmail.com>
2020-11-11 12:50:08 +0100

    Add 'owner' field to project model

--- backend/src/models/project.js
+++ backend/src/models/project.js
@@ -1,15 +1,19 @@
 const mongoose = require('mongoose')
 const autopopulate = require('mongoose-autopopulate')
+const Interest = require('./interest')
 
 const projectSchema = new mongoose.Schema({
   name: {
     type: String,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
     required: true,
   },
   startDate: {
     type: Date,
     unique: false,
-    // required: true,
   },
   note: {
     type: String,

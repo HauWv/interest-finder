0039b027c67b78e38a66ceda33e1c407bcc520cf
Jill-Ann <jillannkay@gmail.com>
2020-11-11 12:49:30 +0100

    Add projects view to frontend

--- frontend/src/views/projects.vue
+++ frontend/src/views/projects.vue
@@ -1,16 +1,33 @@
 <script>
+import axios from 'axios'
+import ProjectCard from '@/components/project-card.vue'
+
+import { mapActions } from 'vuex'
+
 export default {
   name: 'Projects',
-  components: {},
+  components: { ProjectCard },
   data() {
     return {
       projects: []
+      // project: null
     }
+  },
+
+  async created() {
+    this.projects = await this.fetchProjects()
+    // console.log(this.projects)
+  },
+
+  methods: {
+    ...mapActions(['fetchProjects'])
   }
-  // get projects here.
 }
 </script>
 
 <template lang="pug">
-    
+  .box
+    h1 My Projects
+    div(v-for="project in projects" :project="project")
+      p {{project.name}}
 </template>

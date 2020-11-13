3cf407ffd6f9250da4d071a82c3375449b735142
Jill-Ann <jillannkay@gmail.com>
2020-11-13 17:09:26 +0100

    Update register form

--- frontend/src/views/register.vue
+++ frontend/src/views/register.vue
@@ -40,11 +40,11 @@ export default {
     .container.border
         .row
             .col
-                form.p-5(@submit.prevent='signUp(user)')
+                form.p-5(@submit.prevent='submitLogin')
                     h4.mb-4.text-center Sign up
-                    input#defaultRegisterFormFirstName.form-control.mb-3(v-model='user.name' type='text' name='name' placeholder='Name')
-                    input#defaultRegisterFormEmail.form-control.mb-4(v-model='user.email' type='email' name='email' placeholder='E-mail')
-                    input#defaultRegisterFormPassword.form-control(v-model='user.password' type='password' name='password' placeholder='Password' aria-describedby='defaultRegisterFormPasswordHelpBlock')
+                    input#defaultRegisterFormFirstName.form-control.mb-3(v-model='name' type='text' name='name' placeholder='Name')
+                    input#defaultRegisterFormEmail.form-control.mb-4(v-model='email' type='email' name='email' placeholder='E-mail')
+                    input#defaultRegisterFormPassword.form-control(v-model='password' type='password' name='password' placeholder='Password' aria-describedby='defaultRegisterFormPasswordHelpBlock')
                     small#defaultRegisterFormPhoneHelpBlock.form-text.text-muted.mb-4 Min 8 characters 
                     button.btn.btn-info.my-4.btn-block(type='submit') Sign up
                     .text-center

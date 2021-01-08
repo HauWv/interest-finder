5466f45b95e02115d06c0c4c62e6a8711108fb78
Jill-Ann <jillannkay@gmail.com>
2021-01-08 12:24:36 +0100

    Remove calculator file

--- backend/calculator.js
+++
@@ -1,7 +0,0 @@
-function findAverage(numOfPhotos, numOfUsers) {
-  if (typeof numOfPhotos !== 'number' || typeof numOfUsers !== 'number') throw new TypeError()
-  if (numOfUsers <= 0) return 0
-  return numOfPhotos / numOfUsers
-}
-
-module.exports = findAverage

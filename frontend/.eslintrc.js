8e973608993ca433a1b8e58559fbb8c688c9b304
Jill-Ann <jillannkay@gmail.com>
2021-01-08 12:29:19 +0100

    Install Mocha and Cypress plugins

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     node: true
   },
 
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
 
   parserOptions: {
     parser: 'babel-eslint'
@@ -16,9 +16,14 @@ module.exports = {
     'no-debugger': 'off'
   },
 
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended',
-    '@vue/prettier'
+  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
+
+  overrides: [
+    {
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+      env: {
+        mocha: true
+      }
+    }
   ]
-};
+}
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   devServer: {
-    disableHostCheck: true,
+    disableHostCheck: true
   },
 
   lintOnSave: false
-};
+}

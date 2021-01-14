fddcacd13e11c7a23fa2d4b3d3bae852289d8d03
Jill-Ann <jillannkay@gmail.com>
2021-01-14 17:07:20 +0100

    Re-style login form

--- frontend/src/views/login.vue
+++ frontend/src/views/login.vue
@@ -7,7 +7,8 @@ export default {
     return {
       email: '',
       password: '',
-      backendError: null
+      backendError: null,
+      backgroundColor: '#CEECEF'
     }
   },
   methods: {
@@ -43,18 +44,19 @@ export default {
 //-     div Don't have an account yet? <router-link to="/register">Register</router-link>
 //-     a(href='https://backend-5i4olyndpa-ew.a.run.app/api/accounts/auth/facebook') Login with Facebook
 
-.container
+.container(v-bind:style='{ background: backgroundColor}')
   .col
-    .row.vh-100.d-flex.justify-content-center.align-items-center
+    .row.main-row.d-flex.justify-content-center.align-items-center
       .form-container.h-40
-        form.login-form.d-flex.flex-column.m-4.p-5
+        form.login-form.d-flex.flex-column.m-4.p-5(@submit="submitLogin")
           h1.mb-4 Log in
-          input#email.form-elem.my-2.p-2(type='email' placeholder='Email address')
-          input#password.form-elem.my-2.p-2(type='password' placeholder='Password')
+          input#email.form-elem.my-2.p-2(v-model="email" type="email" placeholder="Email address" required)
+          input#password.form-elem.my-2.p-2(v-model="password" type="password" placeholder="Password" required)
           input.form-elem.my-2.p-2(type='submit' value='Log in')
+          div(v-if="backendError") {{ backendError }}
           p.form-elem.my-2.p-1
-            | Don't have an account yet?
-            a(href='/register') Register
+            | Don't have an account yet? 
+            <router-link to="/register">Register</router-link>
           a.form-elem.my-2.p-2.fb(href='https://backend-5i4olyndpa-ew.a.run.app/api/accounts/auth/facebook')
             i.fa.fa-facebook.fa-fw.mr-2
             | Log in with Facebook
@@ -68,14 +70,19 @@ export default {
 // }
 
 body {
-    background-color: #CEECEF !important;
+    background: #CEECEF;
+}
+.main-row {
+  height: 80vh;
 }
 
 .form-container {
-    background-color: #030D4F;
-    border-radius: 2%;
+    width: auto;
+    background-color: white;
+    border-radius: 6%;
+    box-shadow: 5px 10px 20px darkslategray;
 }
-
+ 
 form {
     background-color: white;
 }
@@ -94,13 +101,14 @@ form {
 .fb {
     background-color: #1877F2;
     color: white;
+    font-weight: bold;
     border-radius: 4px;
     font-size: 17px;
-    text-decoration: none;
+    text-decoration: none; 
   }
 
-.fb:hover {
-    color: white;
-    text-decoration: none;
-}
+  .fb:hover {
+      color: white;
+      text-decoration: none;
+  }
 </style>
